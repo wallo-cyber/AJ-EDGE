d97d5e39f88537cc05de280450719683a985d2be
wallo-cyber <waleedbamahriz@gmail.com>
2026-08-17 17:42:51 +0300

    Vendors v2

---
+++ .next/dev/static/chunks/app_1anvha4._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_1mojsay._.js","static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_02adbso._.js","static/chunks/18pm_next_dist_compiled_next-devtools_index_09ncdke.js","static/chunks/18pm_next_dist_compiled_react-dom_1i68fn8._.js","static/chunks/18pm_next_dist_compiled_react-server-dom-turbopack_1dwd26-._.js","static/chunks/18pm_next_dist_compiled_141y_dp._.js","static/chunks/18pm_next_dist_client_06hkw1-._.js","static/chunks/18pm_next_dist_1qal8ok._.js","static/chunks/18pm_@swc_helpers_cjs_1st40pi._.js"],
+    source: "entry"
+});
